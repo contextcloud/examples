ebb43676de53ac72beaa432615418f7802b2477e
Chris Kolenko <chris@webcanvas.com.au>
2022-10-30 19:38:24 +1100

    Moving examples to it's own repo

---
+++ assets_2/handler.go
@@ -0,0 +1,10 @@
+package function
+
+import "net/http"
+
+func NewHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello assets!"))
+	})
+}
